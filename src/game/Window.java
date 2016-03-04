33920bae39af6cd9448779dc3479d550375bbe13
Jens Harder <je.harder@gmail.com>
2016-03-04 14:58:08 +0100

    bugfix

--- src/game/Window.java
+++ src/game/Window.java
@@ -36,6 +36,7 @@ public class Window extends Application {
 	@Override
 	public void start(Stage stage) {
 		lvl = World.getWorld();
+		lvl.changeState(state);
 		Menu.getMenu().setList(new String[]{"Start", "Reset", "Help", "Credits", "Exit"});
 
 		// root objects
