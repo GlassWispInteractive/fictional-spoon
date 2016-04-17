be02800f111e8ebc5beb51d3a64b76d6df4e9d45
Danny <danny.rademacher@gmail.com>
2016-04-17 18:26:30 +0200

    fixes to audio and images

--- src/core/Audio.java
+++ src/core/Audio.java
@@ -5,32 +5,34 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 
-import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class Audio {
-    private static HashMap<String, AudioClip> audio = new HashMap<>();
-    
-    static {
-    	try {
-            Files.walk(Paths.get("res/images")).forEach(filePath -> {
-                if (Files.isRegularFile(filePath)) {
-                    String[] s = filePath.toString().split("\\\\");
-                    String name = s[s.length - 1];
-                    name = name.substring(0, name.lastIndexOf("."));
-                    
-                    audio.put(name, new AudioClip(filePath.toUri().toString()));
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public static void get(String key) {
+	private static HashMap<String, Media> audio = new HashMap<>();
+
+	static {
+		try {
+			Files.walk(Paths.get("res/audio")).forEach(filePath -> {
+				if (Files.isRegularFile(filePath)) {
+					String[] s = filePath.toString().split("\\\\");
+					String name = s[s.length - 1];
+					name = name.substring(0, name.lastIndexOf("."));
+
+					audio.put(name, new Media(filePath.toUri().toString()));
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void play(String key) {
         if (!audio.containsKey(key)) {
-            throw new AssertionError("image does not exist");
+            throw new AssertionError("audio does not exist");
         }
         
-        audio.get(key).play();
+		MediaPlayer player = new MediaPlayer(audio.get(key));
+		player.setAutoPlay(true);
     }
 }
